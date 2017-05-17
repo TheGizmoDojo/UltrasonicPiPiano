c0bab92350035de166494f79f97595e4b1e6a737
Andy Grove <andygrove73@gmail.com>
2017-05-16 20:40:39 -0600

    update run.sh to specify gestures

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cargo build --release
-./target/release/ultrasonic_piano --mode=linear --cm-per-note=2 1 10 18 25 41 89 49 14 | fluidsynth -a alsa -g 1.0 -l /usr/share/sounds/sf2/FluidR3_GM.sf2
+./target/release/ultrasonic_piano --gesture_change_instrument 129 --gesture_shutdown 66 --mode=linear --cm-per-note=2 1 10 18 25 41 89 49 14 | fluidsynth -a alsa -g 1.0 -l /usr/share/sounds/sf2/FluidR3_GM.sf2
